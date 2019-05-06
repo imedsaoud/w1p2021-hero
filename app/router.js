9c88eaabc3e8e9415422b24d87362c2c75a9f29a
imedsa <contact.imedsa@gmail.com>
2019-05-07 00:06:48 +0200

    Added Route components

--- app/router.js
+++ app/router.js
@@ -2,7 +2,10 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
 import Home from './components/Home.vue';
-import Page from './components/Page.vue';
+import Characters from './components/Characters.vue';
+import Game1 from './components/Game1.vue';
+import Win from './components/Win.vue';
+import Lost from './components/Lost.vue';
 
 Vue.use(Router);
 
@@ -16,9 +19,24 @@ const router = new Router({
       component: Home,
     },
     {
-      path: '/page',
-      name: 'page',
-      component: Page,
+      path: '/characters',
+      name: 'characters',
+      component: Characters,
+    },
+    {
+      path: '/game/1',
+      name: '1',
+      component: Game1
+    },
+    {
+      path: '/win',
+      name: 'win',
+      component: Win
+    },
+    {
+      path: '/lost',
+      name: 'lost',
+      component: Lost
     },
     {
       path: '*',
